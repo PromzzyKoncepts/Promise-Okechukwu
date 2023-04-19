d39fb7594fe44ed6f684740af2525f17296f3041
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-04-19 23:14:32 +0100

    add hook to handle scroll to top

---
+++ src/hooks/useScrollToTop.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react';
+import Slide from 'react-reveal/Slide';
+import { FiChevronUp } from 'react-icons/fi';
+
+const useScrollToTop = () => {
+  const [showScroll, setShowScroll] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', scrollToTop);
+    return function cleanup() {
+      window.removeEventListener('scroll', scrollToTop);
+    };
+  });
+
+  const scrollToTop = () => {
+    if (!showScroll && window.pageYOffset > 400) {
+      setShowScroll(true);
+    } else if (showScroll && window.pageYOffset <= 400) {
+      setShowScroll(false);
+    }
+  };
+
+  const backToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  window.addEventListener('scroll', scrollToTop);
+
+  return (
+    <>
+      <Slide bottom>
+        <FiChevronUp
+          className="scrollToTop"
+          onClick={backToTop}
+          style={{
+            height: 45,
+            width: 45,
+            borderRadius: 50,
+            right: 50,
+            transform: 'translate(-50px, -60px)',
+            display: showScroll ? 'flex' : 'none',
+            float: 'right',
+            backgroundColor: '#333',
+            cursor: 'pointer'
+          }}
+        />
+      </Slide>
+    </>
+  );
+};
+
+export default useScrollToTop;
