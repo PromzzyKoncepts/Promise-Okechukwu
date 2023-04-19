aba416aeaef32d5517d0670504c32a589e708b39
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-04-19 14:59:09 +0100

    add Page to handle Home components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Skills from "../components/skills"
+import Slider from "../components/Slider";
+const Home = () => {
+  return (
+    <div>
+      <h1>This is Home</h1>
+      <Slider />
+      <Skills />
+      </div>
+
+  )
+}
+export default Home;
