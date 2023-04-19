6697ba50e594204d86d299d53ada5e6776ebc371
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-04-19 23:30:40 +0100

    Modifications

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,28 +5,10 @@ import { Link } from 'react-router-dom';
 import Fade from 'react-reveal/Fade';
 import { motion } from 'framer-motion';
 import logo from '../assets/images/p.png';
-import HireMeModal from '../modals/hireMe';
 import Button from '../partials/Button';
 import '../assets/Stylesheets/Header.css';
 
 const Header = () => {
-  // const { activeTheme, setTheme } = ThemeSwitcher();
-  const [showModal, setShowModal] = useState(false);
-
-  const showHireMeModal = () => {
-    if (!showModal) {
-      document
-        .getElementsByTagName('html')[0]
-        .classList.add('overflow-y-hidden');
-      setShowModal(true);
-    } else {
-      document
-        .getElementsByTagName('html')[0]
-        .classList.remove('overflow-y-hidden');
-      setShowModal(false);
-    }
-  }
-
   return (
     <motion.nav
       initial={{ opacity: 0 }}
@@ -86,7 +68,7 @@ const Header = () => {
           </div>
 
           {/* Hire me modal */}
-          <div>
+          {/* <div>
             {showModal ? (
               <HireMeModal
                 onClose={showHireMeModal}
@@ -94,41 +76,7 @@ const Header = () => {
               />
             ) : null}
             {showModal ? showHireMeModal : null}
-          </div>
-          {/* Theme switcher small screen */}
-          {/* <div
-          onClick={() => setTheme(activeTheme)}
-          aria-label="Theme Switcher"
-          className=""
-        >
-          {activeTheme === 'dark' ? (
-            <FiMoon className="" />
-          ) : (
-            <FiSun className="" />
-          )}
-        </div> */}
-
-          {/* Small screen hamburger menu */}
-          {/* <div className="sm:hidden">
-          <button
-            // onClick={toggleMenu}
-            type="button"
-            className="focus:outline-none"
-            aria-label="Hamburger Menu"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              className=""
-            >
-              {showMenu ? (
-                <FiX className="text-3xl" />
-              ) : (
-                <FiMenu className="text-3xl" />
-              )}
-            </svg>
-          </button>
-        </div> */}
+          </div> */}
 
         </div>
 
