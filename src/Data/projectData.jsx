90467285b93ab0d9599059b2d1df0e9d5acd8085
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-04-19 23:17:17 +0100

    Add data for project lists

--- src/Data/projectData.jsx
+++ src/Data/projectData.jsx
@@ -1,1 +1,80 @@
-export
+import spaceHub from '../assets/images/Spacehub.png';
+import budgetApp from '../assets/images/BudgetApp.png';
+import covidTracker from '../assets/images/CovidTracker.png';
+import pranMovies from '../assets/images/Pranmovies.png';
+import Regal from '../assets/images/Regal.png';
+
+export default [
+  {
+    id: 1,
+    name: "Regal Rentals",
+    image: Regal,
+    technologies: ["React", "Redux", "Rails", "CSS"],
+    github: "https://github.com/PromzzyKoncepts/Book-Appointment-App",
+    demo: "https://eldorado-rentals.netlify.app",
+  },
+  {
+    id: 2,
+    name: "Budget App",
+    image: budgetApp,
+    technologies: ["Ruby", "Rails", "CSS"],
+    github: "https://github.com/PromzzyKoncepts/Budget-App",
+    demo: "https://budget-app-niq8.onrender.com/",
+  },
+  {
+    id: 3,
+    name: "Recipe App",
+    image: budgetApp,
+    technologies: ["Ruby", "Rails", "CSS"],
+    github: "https://github.com/PromzzyKoncepts/Budget-App",
+    demo: "https://budget-app-niq8.onrender.com/",
+  },
+  {
+    id: 4,
+    name: "Blog App",
+    image: budgetApp,
+    technologies: ["Ruby", "Rails", "CSS"],
+    github: "https://github.com/PromzzyKoncepts/Budget-App",
+    demo: "https://budget-app-niq8.onrender.com/",
+  },
+  {
+    id: 5,
+    name: "SpaceHub App",
+    image: spaceHub,
+    technologies: ["React", "Redux", "API"],
+    github: "https://github.com/PromzzyKoncepts/Space-Travellers-Hub",
+    demo: "spacehubb.netlify.app"
+  },
+  {
+    id: 6,
+    name: "Covid19 Tracker",
+    image: covidTracker,
+    technologies: ["React", "Redux", "API"],
+    github: "https://github.com/PromzzyKoncepts/Covid19-Tracker-App",
+    demo: "https://c0r0natracker.netlify.app/"
+  },
+  {
+    id: 7,
+    name: "BookStore App",
+    image: spaceHub,
+    technologies: ["React", "Redux", "API"],
+    github: "https://github.com/PromzzyKoncepts/BookStore-React",
+    demo: "bookstore-redux.netlify.app",
+  },
+  {
+    id: 8,
+    name: "PranMovies",
+    image: pranMovies,
+    technologies: ["JavaScript", "CSS3", "API"],
+    github: "https://github.com/PromzzyKoncepts/PranMovies",
+    demo: "PromzzyKoncepts.github.io/PranMovies/",
+  },
+  {
+    id: 9,
+    name: "Maths Calculator",
+    image: spaceHub,
+    technologies: ["React", "Javascript", "CSS"],
+    github: "https://github.com/PromzzyKoncepts/Math-Magician",
+    demo: "mathmag.netlify.app",
+  },
+]
