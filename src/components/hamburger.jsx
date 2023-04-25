8098998a70783120552951ce74b7b4614fe21054
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-04-26 00:32:37 +0100

    Add component for menu icon

---
+++ src/components/hamburger.jsx
@@ -0,0 +1,119 @@
+import styled from 'styled-components';
+
+const StyledMenu = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: #282828;
+  transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(-100%)'};
+  height: 100vh;
+  text-align: left;
+  padding: 2rem;
+  position: fixed;
+  z-index: 3;
+  top: 0;
+  left: 0;
+  transition: transform 0.3s ease-in-out;
+
+  @media (max-width: 768px) {
+      width: 100%;
+    }
+
+  a {
+    font-size: 2rem;
+    text-transform: uppercase;
+    padding: 2rem 0;
+    font-weight: bold;
+    letter-spacing: 0.5rem;
+    color: #fff;
+    text-decoration: none;
+    transition: color 0.3s linear;
+
+    @media (max-width: 768px) {
+      font-size: 1.5rem;
+      text-align: left;
+      border-bottom: 1px solid white
+    }
+
+    &:hover {
+      color: #343078;
+    }
+  }
+`
+
+const Menu = ({ open }) => {
+  return (
+    <StyledMenu open={open}>
+      <a href="/">
+        <span role="img" aria-label="Home"></span>
+        Home
+      </a>
+      <a href="/about">
+        <span role="img" aria-label="Home"></span>
+        About us
+      </a>
+      <a href="/contact">
+        <span role="img" aria-label="price"></span>
+        Contact
+      </a>
+    </StyledMenu>
+  )
+}
+
+const StyledBurger = styled.button`
+  position: absolute;
+  top: 5%;
+  left: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 2rem;
+  height: 2rem;
+  background: transparent;
+  border: none;
+  position: fixed;
+  z-index: 3;
+  cursor: pointer;
+  color: white;
+  padding: 0;
+  z-index: 10;
+
+  &:focus {
+    outline: none;
+  }
+
+  div {
+    width: 2rem;
+    height: 0.25rem;
+    background: ${({ open }) => open ? '#fff' : '#fff'};
+    border-radius: 10px;
+    transition: all 0.3s linear;
+    position: relative;
+    transform-origin: 1px;
+
+    :first-child {
+      transform: ${({ open }) => open ? 'rotate(45deg)' : 'rotate(0)'};
+    }
+
+    :nth-child(2) {
+      opacity: ${({ open }) => open ? '0' : '1'};
+      transform: ${({ open }) => open ? 'translateX(20px)' : 'translateX(0)'};
+    }
+
+    :nth-child(3) {
+      transform: ${({ open }) => open ? 'rotate(-45deg)' : 'rotate(0)'};
+    }
+  }
+`
+
+const Burger = ({ open, setOpen }) => {
+  return (
+    <StyledBurger open={open} onClick={() => setOpen(!open)}>
+      <div />
+      <div />
+      <div />
+    </StyledBurger>
+  )
+}
+
+export { Burger, Menu };
