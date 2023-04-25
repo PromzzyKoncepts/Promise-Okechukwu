bad8d1340e7dc322f7cfe44f8c63d966315481ba
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-04-25 21:26:05 +0100

    Add component

---
+++ src/hooks/aboutCounter.jsx
@@ -0,0 +1,43 @@
+import { useCountUp } from 'react-countup';
+import CounterItem from './counterItem';
+import '../assets/Stylesheets/about.css';
+
+
+const AboutCounter = () => {
+  useCountUp({ ref: 'experienceCounter', end: 3, duration: 2 });
+  useCountUp({ ref: 'githubStarsCounter', end: 300, duration: 2 });
+  useCountUp({ ref: 'feedbackCounter', end: 92, duration: 2 });
+  useCountUp({ ref: 'projectsCounter', end: 77, duration: 2 });
+
+  return (
+    <div className="">
+      <div className="counter-container">
+        <CounterItem
+          title="Years of experience"
+          counter={<span id="experienceCounter" />}
+          measurement=""
+        />
+
+        <CounterItem
+          title="Stars on GitHub"
+          counter={<span id="githubStarsCounter" />}
+          measurement="+"
+        />
+
+        <CounterItem
+          title="Positive feedback"
+          counter={<span id="feedbackCounter" />}
+          measurement="%"
+        />
+
+        <CounterItem
+          title="Projects completed"
+          counter={<span id="projectsCounter" />}
+          measurement="%"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AboutCounter;
