5360543299aa6c6bfcab49bd4d83d63e160c5f0b
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-04-26 01:18:07 +0100

    fix linters

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -2,17 +2,17 @@ import skillsData from '../Data/skillsData';
 import '../assets/Stylesheets/Skills.css';
 
 const Skills = () => (
-    <div className="skill-container">
-      <h2>Skills, Languages and Frameworks</h2>
-      <div className="skill-cards">
-        {skillsData.map((item) => (
-          <div className="card" key={item.id}>
-            {item.icon}
-            <p>{item.text}</p>
-          </div>
-        ))}
-      </div>
+  <div className="skill-container">
+    <h2>Skills, Languages and Frameworks</h2>
+    <div className="skill-cards">
+      {skillsData.map((item) => (
+        <div className="card" key={item.id}>
+          {item.icon}
+          <p>{item.text}</p>
+        </div>
+      ))}
     </div>
+  </div>
 
 );
 export default Skills;
